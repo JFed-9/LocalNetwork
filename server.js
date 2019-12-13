72852cdfdaf037954b25896c28b5becbf89e3a85
Jackson Feddock <jackson@xlr8dev.com>
2019-12-12 21:28:04 -0700

    Added first view

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /* Import needed dependencies */
 const express = require('express');
-const session = require('express-session');
 const http = require('http');
 const Q = require('q');
 
@@ -9,7 +8,6 @@ var connectionsArray = []; //used to track clients that connect to the web socke
 
 var app = express(); //create the express app
 var server = http.createServer(app); //create an http server using the express app
-var io = require('socket.io').listen(server); //creates a web socket server that will be used for real time updating. You will use this in part 2 of the lab.
 
 /* This section configures the express app */
 app.engine('ejs', engine); //use ejs as the templating engine
@@ -26,21 +24,6 @@ app.get('/', function (req, res){
 	res.render('index');
 })
 
-//Socket handling
-io.sockets.on('connection', function (socket){
-	connectionsArray.push(socket);
-	console.log('A new socket has connected, total connected: ', connectionsArray.length);
-
-	//sets up a handler for the disconnect event
-	socket.on('disconnect', function (){
-		var socketIndex = connectionsArray.indexOf(socket);
-		if (socketIndex >= 0){
-			connectionsArray.splice(socketIndex, 1);
-			console.log('socket ' + socketIndex + ' disconnected');
-		}
-	})
-})
-// Tell the Browser to stop asking for the favicon :)
 app.get('/favicon.ico', function(req, res) {
     res.redirect('/assets/favicon.ico');
 });
