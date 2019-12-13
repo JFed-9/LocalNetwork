95daf647d9a8affd8129b9a450b863d5f9115af5
Jackson Feddock <jackson@xlr8dev.com>
2019-12-12 22:04:05 -0700

    dbg

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var server = http.createServer(app); //create an http server using the express a
 app.engine('ejs', engine); //use ejs as the templating engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+var dir =  process.cwd();
+app.use(express.static(dir)); //current working directory
 app.use(express.static(__dirname + '/assets'));
 
 /* This starts your http server on the given port */
@@ -27,6 +29,42 @@ app.get('/index', function(req, res){
 app.get('/', function (req, res){
 	res.redirect('/index');
 })
+
+app.get('/files', function(req, res) {
+ var currentDir =  dir;
+ var query = req.query.path || '';
+ if (query) currentDir = path.join(dir, query);
+ console.log("browsing ", currentDir);
+ fs.readdir(currentDir, function (err, files) {
+     if (err) {
+        throw err;
+      }
+      var data = [];
+      files
+      .forEach(function (file) {
+        try {
+                //console.log("processing ", file);
+                var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
+                if (isDirectory) {
+                  data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
+                } else {
+                  var ext = path.extname(file);
+                  if(program.exclude && _.contains(program.exclude, ext)) {
+                    console.log("excluding file ", file);
+                    return;
+                  }
+                  data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
+                }
+
+        } catch(e) {
+          console.log(e);
+        }
+
+      });
+      data = _.sortBy(data, function(f) { return f.Name });
+      res.json(data);
+  });
+});
 
 app.get('/favicon.ico', function(req, res) {
     res.redirect('/assets/favicon.ico');
